e4a14db0765e4dd2a88258206f0a3daf3e9b809f
Camilo Enrique Uribe Navas <61896434+imcamilouribe@users.noreply.github.com>
2023-11-17 14:45:39 -0500

    Starting new test

---
+++ codewar/src/main/java/com/practice/codewar/scramblies/implement/scrambliesImpl.java
@@ -0,0 +1,5 @@
+package com.practice.codewar.scramblies.implement;
+
+public class scrambliesImpl {
+
+}
---
+++ codewar/src/main/java/com/practice/codewar/scramblies/service/scrambliesService.java
@@ -0,0 +1,6 @@
+package com.practice.codewar.scramblies.service;
+
+public interface scrambliesService {
+
+	public boolean scramble(String str1, String str2);
+}
