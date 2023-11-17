73836619348368fb4adfdfffdf8a46525c798476
Camilo Enrique Uribe Navas <61896434+imcamilouribe@users.noreply.github.com>
2023-11-17 14:44:55 -0500

    Delete scramblies directory

--- scramblies/implement/scrambliesImpl.java
+++
@@ -1,5 +0,0 @@
-package com.practice.codewar.scramblies.implement;
-
-public class scrambliesImpl {
-
-}
--- scramblies/service/scrambliesService.java
+++
@@ -1,6 +0,0 @@
-package com.practice.codewar.scramblies.service;
-
-public interface scrambliesService {
-
-	public boolean scramble(String str1, String str2);
-}
