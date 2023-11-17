902c5b993c08d634df40a733316fe0a546088d2b
Camilo Enrique Uribe Navas <61896434+imcamilouribe@users.noreply.github.com>
2023-11-17 15:19:09 -0500

    Updating

--- codewar/src/main/java/com/practice/codewar/scramblies/implement/scrambliesImpl.java
+++ codewar/src/main/java/com/practice/codewar/scramblies/implement/scrambliesImpl.java
@@ -1,5 +1,24 @@
 package com.practice.codewar.scramblies.implement;
 
-public class scrambliesImpl {
+import com.practice.codewar.scramblies.service.scrambliesService;
+
+public class scrambliesImpl implements scrambliesService{
+
+	@Override
+	public boolean scramble(String str1, String str2) {
+	
+		boolean canScramble = true;
+		 String toCompare = str1.length() > str2.length()? str2 : str1;
+	    char[] letters = str1.length() > str2.length()? str1.toCharArray() : str2.toCharArray();
+		  
+		  for(char letter: letters){
+		    if (!toCompare.contains(String.valueOf(letter))) {
+		    	canScramble = false;
+		    	break;
+		    }
+		  }
+	      
+			return canScramble;
+	}
 
 }
